2f8395d15aad8c6b14eadda9eafbd1907a05ea74
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2025-04-26 22:36:41 +0600

    react bits

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import { useSpring, animated } from "@react-spring/web";
+import { useState } from "react";
+import ShinyText from "./components/ShinyText";
 
 const App = () => {
+  const [text, setText] = useState("Munna+Moni");
+
+  const springs = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: { duration: 500 },
+  });
+
   return (
     <div>
-      <h1 className="p-5 bg-lime-200 text-center text-3xl">
-        React Bits Text Animation Component Library.
-      </h1>
+      <div>
+        <h2>Header</h2>
+        <ShinyText
+          text="Just some shiny text!"
+          disabled={false}
+          speed={3}
+          className="custom-class"
+        />
+      </div>
+      <div>
+        <p className="border border-red-300">
+          {text.split("").map((char, index) => (
+            <animated.span key={index} style={springs}>
+              {char}
+            </animated.span>
+          ))}
+        </p>
+      </div>
     </div>
   );
 };
