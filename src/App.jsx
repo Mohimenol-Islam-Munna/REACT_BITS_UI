0a47bc050ea4784d6d8bedc5d434e2eea2b39e72
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2025-04-26 20:23:05 +0600

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="p-5 bg-lime-200 text-center text-3xl">
+        React Bits Text Animation Component Library.
+      </h1>
+    </div>
+  );
+};
+
+export default App;
